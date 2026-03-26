a397303e22d8536ba254de19520e52e5b64ab40a
99acresMohan <jmdproperty@gmail.com>
2026-03-26 07:48:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -121,7 +121,7 @@ app.delete('/api/properties/:id', async (req, res) => {
 
 // --- 4. ATLAS CONNECTION & SERVER START ---
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Encoded password for 'Mohan@93777'
 const encodedPassword = "Mohan%4093777";
